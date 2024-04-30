9475e4463e691da3f3177610d8288eda4cbc9d5d
ashwhi06 <ashe.white06@gmail.com>
2024-04-30 18:59:29 -0400

    syncing repo

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,21 @@
 # code your iterative algorithm here
 
+def binary_search(lst, target):
+    first  = 0
+    last = len(lst) - 1
+    found = False
+    
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if lst[midpoint] == target:
+            found = True
+        else:
+            if lst[midpoint] < target:
+                first = midpoint + 1
+            else:
+                last = midpoint - 1
+    return found
+
+test_list = [5, 8, 12, 14, 19, 22, 27, 30, 31]
+
+print("The binary search results return as:",binary_search(test_list, 12))
